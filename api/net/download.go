15f819b94f678f249236ffbfcccce2e58de9e90a
Taylor Starfield <taylor224@users.noreply.github.com>
2022-12-01 12:22:43 +0900

    Update download.go

--- api/net/download.go
+++ api/net/download.go
@@ -12,30 +12,6 @@ import (
 	"github.com/alfg/openencoder/api/types"
 )
 
-func MoveFile(sourcePath, destPath string) error {
-    inputFile, err := os.Open(sourcePath)
-    if err != nil {
-        return fmt.Errorf("Couldn't open source file: %s", err)
-    }
-    outputFile, err := os.Create(destPath)
-    if err != nil {
-        inputFile.Close()
-        return fmt.Errorf("Couldn't open dest file: %s", err)
-    }
-    defer outputFile.Close()
-    _, err = io.Copy(outputFile, inputFile)
-    inputFile.Close()
-    if err != nil {
-        return fmt.Errorf("Writing to output file failed: %s", err)
-    }
-    // The copy was successful, so now delete the original file
-    err = os.Remove(sourcePath)
-    if err != nil {
-        return fmt.Errorf("Failed removing original file: %s", err)
-    }
-    return nil
-}
-
 // Download downloads a job source based on the driver setting.
 func Download(job types.Job) error {
 	db := data.New()
